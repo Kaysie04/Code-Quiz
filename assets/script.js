a61a65469073c45efb05e084bfb87f023a1c3c3a
Kaysie <kaysielandersonwork@gmail.com>
2022-05-25 12:00:39 -0500

    attempting to make buttons work

--- assets/script.js
+++ assets/script.js
@@ -6,29 +6,34 @@ var questionTwo = document.querySelector("#question-2")
 var questionThree = document.querySelector("#question-3")
 var questionFour = document.querySelector("#question-4")
 var questionFive = document.querySelector("#question-5")
+var finalSection = document.querySelector(".final-section")
 var wrong = document.querySelectorAll("#wrong-answer")
 var correct = document.querySelectorAll("#correct-answer")
-var questionArray = [questionOne, questionTwo, questionThree, questionFour, questionFive]
-
-
-
-
-
-
-
+var questionArray = [questionOne, questionTwo, questionThree, questionFour, questionFive, finalSection]
 
     // add event listeners for wrong answer choices
 
-    wrong.addEventListener("click", wrongChoice)
-    
-    function wrongChoice () {
+   function wrongAnswerAlert () { 
+        wrong.addEventListener("click", wrongChoice())
+        function wrongChoice () {
         alert ("That answer was incorrect")
+        }
     }
 
+
     // add event listener for correct answer choices
-    correct.addEventListener("click", correctChoice)
-    function correctChoice () {
+
+    function correctAnswerAlert () {
+        correct.addEventListener("click", correctChoice())
+        function correctChoice () {
         alert ("That answer was CORRECT!")
+        }
+    }
+
+    // generate quiz 
+    function generateQuiz (questionArray) {
+        for ( i=0; i < questionArray.length; i++) {
+        }
     }
 
 
