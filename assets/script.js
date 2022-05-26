fd9c3bbd860ce2f8422d1adf638d59a494fad18b
Kaysie <kaysielandersonwork@gmail.com>
2022-05-25 19:31:02 -0500

    added variables and functions and event listeners

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,95 @@
 
-// define all const / var
-const body = document.querySelector("body")
-var questionOne = document.querySelector("#question-1")
-var questionTwo = document.querySelector("#question-2")
-var questionThree = document.querySelector("#question-3")
-var questionFour = document.querySelector("#question-4")
-var questionFive = document.querySelector("#question-5")
+var submitBtn = document.querySelector("#start-btn")
+var questionSection = document.querySelector(".question-section")
 var finalSection = document.querySelector(".final-section")
-var wrong = document.querySelectorAll("#wrong-answer")
-var correct = document.querySelectorAll("#correct-answer")
-var questionArray = [questionOne, questionTwo, questionThree, questionFour, questionFive, finalSection]
 
-    // add event listeners for wrong answer choices
 
-   function wrongAnswerAlert () { 
-        wrong.addEventListener("click", wrongChoice())
-        function wrongChoice () {
-        alert ("That answer was incorrect")
-        }
+// set question variables
+var questionOne = {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["A. strings " , "B. booleans" , "C. alerts" , "D. numbers"],
+    answer: "C. alerts"
+}
+var questionTwo = {
+    question: "String values must be enclosed within __________ when being associated with variables.",
+    choices: ["A. commas", "B. curly brackets", "C. quotes", "D. parenthesis"],
+    answer: "C. quotes"
+}
+
+var questionThree = { 
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["A. javascript", "B. terminal/bash", "C. for loops", "D. console.log"],
+    answer: "D. console.log"
+}
+var questionFour = {
+    question:"The condition in an if / else statement is enclosed with",
+    choices: ["A. quotes", "B. parenthesis", "C. curly brackets", "D. square brackets"],
+    answer: "C. curly brackets",
+    
+}
+var questionFive = {
+    question: "Arrays in JavaScript can be used to store _________.",
+    choices:["A. numbers and strings", "B. other arrays", "C. booleans", "D. all of the above"],
+    answer: "D. all of the above",
+}
+
+var questionArray = [questionOne, questionTwo, questionThree, questionFour, questionFive]
+var i = 0
+
+// start quiz function
+
+var quizStart = function () {
+
+    var questionOneDiv = document.createElement("div")
+    questionOneDiv.textContent = questionArray[i].question
+    questionSection.append(questionOneDiv)
+
+        for ( 
+            j=0; j < questionArray[i].choices.length;  j++) {
+            var choicesDiv = document.createElement("button")
+            choicesDiv.textContent = questionArray[i].choices[j]
+            choicesDiv.addEventListener("click" , answerValidation)
+            questionSection.append(choicesDiv)
+        }   
+}
+    
+var answerValidation = function (event) { 
+    if (event.target.textContent === questionArray[i].answer) {
+        alert("CORRECT!")
+
+    }else{
+        alert("Incorrect, time has been subracted by 10 seconds")
     }
+    
+     if  (i===5) {
+         break;
+     }else{
+    i++
+    questionSection.innerHTML = ""
+     quizStart ()   
+     }     
+};
 
+// end quiz section
 
-    // add event listener for correct answer choices
+var endQuiz = function () {
 
-    function correctAnswerAlert () {
-        correct.addEventListener("click", correctChoice())
-        function correctChoice () {
-        alert ("That answer was CORRECT!")
-        }
-    }
+    var finalSection = document.querySelector(".final-section")
+    quizCompleteDiv = document.createElement("div")
+    quizCompleteDiv.textContent = "Quiz Complete!"
+    finalSection.append(quizCompleteDiv)
+    scoreDiv = document.createElement("div")
+    scoreDiv.textContent = "Your final score is:"
+    finalSection.append(scoreDiv)
+    initialsDiv = document.createElement("div")
+    initialsDiv.textContent = "Enter your initials"
+    finalSection.append(initialsDiv)
+    submitBtnDiv = document.createElement("button")
+    finalSection.append(submitBtnDiv)
 
-    // generate quiz 
-    function generateQuiz (questionArray) {
-        for ( i=0; i < questionArray.length; i++) {
-        }
-    }
+    endQuiz ()
+}
 
-
+submitBtn.addEventListener("click" , quizStart)
 
 
