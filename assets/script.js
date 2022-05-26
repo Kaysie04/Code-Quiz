9ca57c1a4a574e7cc87df41a03a8b4206415dae5
Kaysie <kaysielandersonwork@gmail.com>
2022-05-26 09:32:38 -0500

    added setInterval

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,19 @@
+    // timer interval
+    var time = 60
+    setInterval(function() {
+        var timer = document.getElementById("#time-display")
+        time = time - 1
+        timer.textContent = time
+    }, 1000)
+
 
 // style buttons
-document.querySelector("button").style.background= "#79d2a6";
+document.querySelector("button").style.background = "#79d2a6";
 //choicesDiv.style.background = "#79d2a6";
 //choicesDiv.style.border-radius = 10px;
 //width: 150px; 
 
+// set html section variables
 var submitBtn = document.querySelector("#start-btn")
 var questionSection = document.querySelector(".question-section")
 var finalSection = document.querySelector(".final-section")
@@ -13,7 +22,7 @@ var finalSection = document.querySelector(".final-section")
 // set question variables
 var questionOne = {
     question: "Commonly used data types DO NOT include:",
-    choices: ["A. strings " , "B. booleans" , "C. alerts" , "D. numbers"],
+    choices: ["A. strings ", "B. booleans", "C. alerts", "D. numbers"],
     answer: "C. alerts"
 }
 var questionTwo = {
@@ -22,58 +31,68 @@ var questionTwo = {
     answer: "C. quotes"
 }
 
-var questionThree = { 
+var questionThree = {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
     choices: ["A. javascript", "B. terminal/bash", "C. for loops", "D. console.log"],
     answer: "D. console.log"
 }
 var questionFour = {
-    question:"The condition in an if / else statement is enclosed with",
+    question: "The condition in an if / else statement is enclosed with",
     choices: ["A. quotes", "B. parenthesis", "C. curly brackets", "D. square brackets"],
     answer: "C. curly brackets",
-    
+
 }
 var questionFive = {
     question: "Arrays in JavaScript can be used to store _________.",
-    choices:["A. numbers and strings", "B. other arrays", "C. booleans", "D. all of the above"],
+    choices: ["A. numbers and strings", "B. other arrays", "C. booleans", "D. all of the above"],
     answer: "D. all of the above",
 }
 
 var questionArray = [questionOne, questionTwo, questionThree, questionFour, questionFive]
 var i = 0
 
+
 // start quiz function
 
 var quizStart = function () {
 
+
+    if (i > 4) {
+        endQuiz()
+        return;
+    }
+    console.log({ i })
     var questionOneDiv = document.createElement("div")
     questionOneDiv.textContent = questionArray[i].question
     questionSection.append(questionOneDiv)
 
-        for ( 
-            j=0; j < questionArray[i].choices.length;  j++) {
-            var choicesDiv = document.createElement("button")
-            choicesDiv.textContent = questionArray[i].choices[j]
-            choicesDiv.addEventListener("click" , answerValidation)
-            questionSection.append(choicesDiv)
-        }   
+    for (
+        j = 0; j < questionArray[i].choices.length; j++) {
+        var choicesDiv = document.createElement("button")
+        choicesDiv.textContent = questionArray[i].choices[j]
+        choicesDiv.addEventListener("click", answerValidation)
+        questionSection.append(choicesDiv)
+    }
+
 }
-    // check aswer section
-var answerValidation = function (event) { 
-    if (event.target.textContent === questionArray[i].answer) {
+// check aswer section
+var answerValidation = function (event) {
+
+    if (i > 4) return;
+
+    if (event.target.textContent === questionArray[i].answer && i < 5) {
         alert("CORRECT!")
 
-    }else{
-        alert("Incorrect, time has been subracted by 10 seconds")
     }
-    
-     //if  (i===5) {
-        // break;
-    // }else{
+    else {
+        alert("Incorrect, time has been subracted by 10 seconds")
+        time -=10
+    }
+
     i++
     questionSection.innerHTML = ""
-     quizStart ()   
-       
+    quizStart()
+
 };
 
 // end quiz section
@@ -92,10 +111,9 @@ var endQuiz = function () {
     finalSection.append(initialsDiv)
     submitBtnDiv = document.createElement("button")
     finalSection.append(submitBtnDiv)
-
-    endQuiz ()
+    console.log("this is working")
 }
 
-submitBtn.addEventListener("click" , quizStart)
+submitBtn.addEventListener("click", quizStart, setInterval)
 
 
