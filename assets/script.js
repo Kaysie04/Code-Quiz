9c176c379b5e9e5cbe6524ef2db8338eda22c7a9
Kaysie <kaysielandersonwork@gmail.com>
2022-05-27 17:47:24 -0500

    save user input via localStorage

--- assets/script.js
+++ assets/script.js
@@ -1,229 +1,236 @@
-    
-
 // remove title page when quiz starts
 
 function titleDisplay() {
-    var quizTitleDisplay  = document.getElementById("quiz-title")
-    quizTitleDisplay.style.display = "none"
+  var quizTitleDisplay = document.getElementById("quiz-title");
+  quizTitleDisplay.style.display = "none";
 }
 
-   // timer interval
+// timer interval
 
-    var time = 60
-    function setTimer() {
-        setInterval(function() {
-            var timer = document.getElementById("time-display")
-            if (time > 0 && i < 5 ) {
-            time = time - 1
-            timer.textContent = "Time Remaining: " + time
-
-
-            } //else {
-            //endQuiz()
-           //}
-        }, 1000)
-    }
-
-    
+var time = 60;
+function setTimer() {
+  setInterval(function () {
+    var timer = document.getElementById("time-display");
+    if (time > 0 && i < 5) {
+      time = time - 1;
+      timer.textContent = "Time Remaining: " + time;
+    } //else {
+    //endQuiz()
+    //}
+  }, 1000);
+}
 
 // set html document variables
 
-var submitBtn = document.querySelector("#start-btn")
-var questionSection = document.querySelector(".question-section")
-var finalSection = document.querySelector(".final-section")
-var highScoreSection = document.querySelector(".highscore-page")
-var enterResultsBtn = document.getElementById("end-btn")
-var returnToQuizBtn = document.getElementById("refresh-btn")
-var clearScoreBtn = document.getElementById("clear-data-btn")
-
+var submitBtn = document.querySelector("#start-btn");
+var questionSection = document.querySelector(".question-section");
+var finalSection = document.querySelector(".final-section");
+var highScoreSection = document.querySelector(".highscore-page");
+var returnToQuizBtn = document.getElementById("refresh-btn");
+var clearScoreBtn = document.getElementById("clear-data-btn");
+var enterResultsBtn = document.getElementById("end-btn");
+var input = document.getElementById("input-box")
 
 // set question variables
 
 var questionOne = {
-    question: "Commonly used data types DO NOT include:",
-    choices: ["A. strings ", "B. booleans", "C. alerts", "D. numbers"],
-    answer: "C. alerts"
-}
+  question: "Commonly used data types DO NOT include:",
+  choices: ["A. strings ", "B. booleans", "C. alerts", "D. numbers"],
+  answer: "C. alerts",
+};
 var questionTwo = {
-    question: "String values must be enclosed within __________ when being associated with variables.",
-    choices: ["A. commas", "B. curly brackets", "C. quotes", "D. parenthesis"],
-    answer: "C. quotes"
-}
+  question:
+    "String values must be enclosed within __________ when being associated with variables.",
+  choices: ["A. commas", "B. curly brackets", "C. quotes", "D. parenthesis"],
+  answer: "C. quotes",
+};
 
 var questionThree = {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: ["A. javascript", "B. terminal/bash", "C. for loops", "D. console.log"],
-    answer: "D. console.log"
-}
+  question:
+    "A very useful tool used during development and debugging for printing content to the debugger is:",
+  choices: [
+    "A. javascript",
+    "B. terminal/bash",
+    "C. for loops",
+    "D. console.log",
+  ],
+  answer: "D. console.log",
+};
 var questionFour = {
-    question: "The condition in an if / else statement is enclosed with",
-    choices: ["A. quotes", "B. parenthesis", "C. curly brackets", "D. square brackets"],
-    answer: "C. curly brackets",
-
-}
+  question: "The condition in an if / else statement is enclosed with",
+  choices: [
+    "A. quotes",
+    "B. parenthesis",
+    "C. curly brackets",
+    "D. square brackets",
+  ],
+  answer: "C. curly brackets",
+};
 var questionFive = {
-    question: "Arrays in JavaScript can be used to store _________.",
-    choices: ["A. numbers and strings", "B. other arrays", "C. booleans", "D. all of the above"],
-    answer: "D. all of the above",
-}
+  question: "Arrays in JavaScript can be used to store _________.",
+  choices: [
+    "A. numbers and strings",
+    "B. other arrays",
+    "C. booleans",
+    "D. all of the above",
+  ],
+  answer: "D. all of the above",
+};
 
-var questionArray = [questionOne, questionTwo, questionThree, questionFour, questionFive]
-var i = 0
-
+var questionArray = [
+  questionOne,
+  questionTwo,
+  questionThree,
+  questionFour,
+  questionFive,
+];
+var i = 0;
 
 // start quiz function
 
 var quizStart = function () {
-    titleDisplay()
-    if (i === 0) {
-        setTimer()
-    }
+  titleDisplay();
+  if (i === 0) {
+    setTimer();
+  }
 
-    if (i === 5) {
-        endQuiz()
-        return;
-    }
-    var questionOneDiv = document.createElement("div")
-    questionOneDiv.textContent = questionArray[i].question
-    questionOneDiv.className = "question-div"
-    questionSection.append(questionOneDiv)
+  if (i === 5) {
+    endQuiz();
+    return;
+  }
+  var questionOneDiv = document.createElement("div");
+  questionOneDiv.textContent = questionArray[i].question;
+  questionOneDiv.className = "question-div";
+  questionSection.append(questionOneDiv);
 
-    for (
-        j = 0; j < questionArray[i].choices.length; j++) {
-        var choicesDiv = document.createElement("button")
-        choicesDiv.textContent = questionArray[i].choices[j]
-        choicesDiv.className = "button-style"
-        choicesDiv.addEventListener("click", answerValidation)
-        questionSection.append(choicesDiv)
-    
-    }
-
-}
-// check aswer section
+  for (j = 0; j < questionArray[i].choices.length; j++) {
+    var choicesDiv = document.createElement("button");
+    choicesDiv.textContent = questionArray[i].choices[j];
+    choicesDiv.className = "button-style";
+    choicesDiv.addEventListener("click", answerValidation);
+    questionSection.append(choicesDiv);
+  }
+};
+// check answer section
 var answerValidation = function (event) {
+  if (i > 4) return;
 
-    if (i > 4) return;
+  if (event.target.textContent === questionArray[i].answer && i < 5) {
+    alert("CORRECT!");
+  } else {
+    alert("Incorrect, time has been subracted by 5 seconds");
+    time -= 5;
+  }
 
-    if (event.target.textContent === questionArray[i].answer && i < 5) {
-        alert("CORRECT!")
+  i++;
+  questionSection.innerHTML = "";
+  quizStart();
+};
 
-    }
-    else {
-        alert("Incorrect, time has been subracted by 5 seconds")
-        time -=5
-    }
+//quiz complete section
+    
+function endQuiz() {
+  clearInterval(setTimer);
 
-    i++
-    questionSection.innerHTML = ""
-    quizStart()
+  // remove questions when quiz is over
+  var questionDisplay = document.getElementById("question-id");
+  questionDisplay.style.display = "none";
+
+  // remove display:none from CSS
+  var displayFinalSection = document.getElementsByClassName("final-section");
+  finalSection.classList.remove("final-section");
+
+  // remove timer display
+  var timerDisplay = document.getElementById("time-display");
+  timerDisplay.style.display = "none";
+
+  // the final score needs to be displayed
+  var finalScoreSection = document.getElementById("score");
+  var finalScoreDiv = document.createElement("div");
+  finalScoreDiv.textContent = time;
+  finalScoreSection.append(finalScoreDiv);
 }
 
-    //quiz complete section
+function userInput() {
+    localStorage.setItem(time, input.value)
+    }
 
-function endQuiz () {
-    clearInterval(setTimer)
-
-    // remove questions when quiz is over
-    var questionDisplay = document.getElementById("question-id")
-    questionDisplay.style.display = "none"
-
-    // remove display:none from CSS
-    var displayFinalSection = document.getElementsByClassName("final-section")
-    finalSection.classList.remove("final-section")
-
-    // remove timer display
-    var timerDisplay = document.getElementById("time-display")
-    timerDisplay.style.display = "none"
+// user input function
 
 
-    // add input for initials at the end of the game
-    var input = document.createElement("input")
-    input.setAttribute('type','text')
-    var enterInitials = document.getElementById("initials")
-    enterInitials.append(input)
 
-    // the final score needs to be displayed
-    var finalScoreSection = document.getElementById("score")
-    var finalScoreDiv = document.createElement("div")
-    finalScoreDiv.textContent = time
-    finalScoreSection.append(finalScoreDiv)
-    
+// ENTER RESULTS SECTION
 
-    // when I click the enter results-btn I want the input to be saved
+// highscore section will now display ////////
+// I want the quiz complete display to go away ///////
+// time display removed ////////
 
-    //  when I click the enter results button it goes to the enter results function
-  }
+// I want the page to display the high scores
 
-   // ENTER RESULTS SECTION 
+// restyle the highscore page
 
-     // highscore section will now display ////////
-    // I want the quiz complete display to go away ///////
-    // time display removed ////////
-    
-    // I want the page to display the high scores
-   
-    // restyle the highscore page
-    
-
-    function enterResults() {
-    
-    var displayHighScoreSection = document.getElementsByClassName("highscore-page")
-    highScoreSection.classList.remove("highscore-page")
-
-    var resultDisplay = document.getElementById("result-section")
-    resultDisplay.style.display = "none"
-  }
+function enterResults() {
   
+    var displayHighScoreSection =
+    document.getElementsByClassName("highscore-page");
+    highScoreSection.classList.remove("highscore-page");
 
+    var resultDisplay = document.getElementById("result-section");
+    resultDisplay.style.display = "none";
 
+    var highScoreOl = document.getElementById("highscore-ol")
+    var highScoreLi = document.createElement("li")
+    highScoreLi.textContent = userInput().value;
+    highScoreOl.append(highScoreLi);
 
+}
 
-    // clear highscores will delete the input data but stay on current page
-    
-    // clear highscore button that will delete the input data
+// button sections
 
-  
+// button that starts the quiz
+submitBtn.addEventListener("click", quizStart);
 
-    
-  // button sections 
-
-  // button that starts the quiz
-  submitBtn.addEventListener("click", quizStart)
-  
-  // button that saves the initial input
- enterResultsBtn.addEventListener("click", enterResults)
+// button that saves the user input
+enterResultsBtn.addEventListener("click", userInput);
 
 // return quiz button will display the start quiz page
 document.getElementById("refresh-btn").onclick = function () {
-    location.href = "https://kaysie04.github.io/Code-Quiz/";  
+  location.href = "https://kaysie04.github.io/Code-Quiz/";
 };
 
 
 
 
+// function getUserName() {
+//     var nameField = document.getElementById('nameField').value;
+//     var result = document.getElementById('result');
+    
+//     if (nameField.length < 3) {
+//         result.textContent = 'Username must contain at least 3 characters';
+//         //alert('Username must contain at least 3 characters');
+//     } else {
+//         result.textContent = 'Your username is: ' + nameField;
+//         //alert(nameField);
+//     }
+// }
+    
 
 
 
 
 
+
+// clear highscores will delete the input data but stay on current page
+
+// clear highscore button that will delete the input data
 
 //var user = {
-    //firstName: firstNameInput.value.trim(),
-    //lastName: lastNameInput.value.trim(),
-    //email: emailInput.value.trim(),
+//firstName: firstNameInput.value.trim(),
+//lastName: lastNameInput.value.trim(),
+//email: emailInput.value.trim(),
 //password: passwordInput.value.trim()
-  //};
- 
- 
-  
+//};
 
-
-  // set new submission to local storage 
- // localStorage.setItem("user",JSON.stringify(user))
- //});
-  
-
-
-
-
-
+// set new submission to local storage
+// localStorage.setItem("user",JSON.stringify(user))
+//});
